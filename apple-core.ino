72b0a6dd40e603a39400babf68a237a557b86283
Kenny Luong <luong97@hawaii.edu>
2015-09-22 17:59:18 -1000

    Added lib headers to core ino file

--- apple-core.ino
+++ apple-core.ino
@@ -1,10 +1,26 @@
-/* Apple core file */
+/*
+ * Apple Core
+ *
+ */
+
+/* Include program libs */
+#include "apple.h"
+#include "sensors.h"
+
+/* Include arduino libs */
+#include <Wire.h>
+
+/* Include external libs */
+#include <SHT1x.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_BMP085.h>
 
 void setup(){
 
 }
 
 void loop(){
-
 
 }
